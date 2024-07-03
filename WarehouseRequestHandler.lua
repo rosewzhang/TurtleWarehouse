b28267c0cc3a7961a5a430dbc027ec743b798baf
rosewzhang <92077075+rosewzhang@users.noreply.github.com>
2024-07-03 11:45:26 -0700

    stuff

--- WarehouseRequestHandler.lua
+++ WarehouseRequestHandler.lua
@@ -52,28 +52,39 @@ end
 
 -- TODO: try to make this error in bugtesting
 function WarehouseRequestHandler_processMessage(self, message)
+    print('HERE00')
     if not message then return end
+    print('HERE10')
     if string.sub(message, 1, 17) ~= 'turtle_warehouse ' then return end
+    print('HERE20')
     -- possibly a valid request, set up to extract the data
     local requestID, requestType, itemID, quantity, port
     -- try to extract request id
     requestID = string.sub(message, 18, 25)
     if string.sub(message, 26, 26) ~= ' ' then return end
+    print('HERE30')
     -- try to extract the type
     requestType = string.sub(message, 27, 27)
     if requestType ~= 'w' and requestType ~= 'd' and requestType ~= 'q' and requestType ~= 'l' then return end
     -- now try to extract the quantity
     if string.sub(message, 28, 28) ~= ' ' then return end
-    quantity = tonumber(string.sub(message, 20, 23))
+    print('HERE40')
+    -- try to extract the type
+    quantity = tonumber(string.sub(message, 30, 33))
     if quantity == nil then return end
     if string.sub(message, 33, 33) ~= ' ' then return end
+    print('HERE50')
     -- now try to extract the port
     port = tonumber(string.sub(message, 34, 37))
     if port == nil then return end
+    print('HERE60')
     if quantity > 9999 or quantity < 0 then return end 
+    print('HERE70')
     if port > 9999 or port < 0 then return end 
+    print('HERE80')
     -- try to extract the itemID
     if string.sub(message, 38, 38) ~= ' ' then return end
+    print('HERE90')
     itemID = string.sub(message, 39, -1)
 
     -- we have all the information. If we've made it to this stage, then the order is valid, so
@@ -94,11 +105,16 @@ function WarehouseRequestHandler_fulfillDepositRequest(self, requestID, quantity
 end
 
 function WarehouseRequestHandler_fulfillWithdrawRequest(self, requestID, itemid, quantity, port)
+    print('HERE100')
+    print(requestID)
+    print(itemid)
+    print(quantity)
+    print(port)
     TurtleWarehouse_withdraw(self.turtleWarehouse, itemid, quantity, port)
 end
 
 function WarehouseRequestHandler_fulfillQueryRequest(self, requestID, itemid, quantity, port)
-    local message = 'turtle_warehouse q '..requestID..' '..quantity..' '..port..' '..'query requests not yet implemented'
+    local message = 'turtle_warehouse '..requestID.. ' q '..quantity..' '..port..' '..'query requests not yet implemented'
     self.modem.transmit(self.outChannel, self.inChannel, message)
     print('query requests not yet implemented')
     -- TODO: implement query requests
@@ -131,7 +147,7 @@ function isSubstring(a, b)
 end
 
 function WarehouseRequestHandler_fulfillListRequest(self, requestID, itemid, quantity, port)
-    local message = 'turtle_warehouse l '..requestID..' '..toFourDigits(quantity)..' '..toFourDigits(port)..' \n'
+    local message = 'turtle_warehouse '..requestID..' l '..toFourDigits(quantity)..' '..toFourDigits(port)..' \n'
     local itemsDict = self.turtleWarehouse.quantities 
     for k, v in pairs(itemsDict) do
         if isSubstring(itemid, k) then
